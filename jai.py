fac4065c8621805c9d2f8caf4c7679efe7425354
kameshchitimoju <chitimoju.kamesh@gmail.com>
2019-07-26 16:42:42 +0530

    Update jai.py

--- jai.py
+++ jai.py
@@ -6,3 +6,50 @@
 
 "jai"
 
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+%matplotlib inline
+
+#D:\python\Prob\UserIdToGenderTrain.csv
+#D:\python\Prob\UserIdToGenderTest.csv
+#D:\python\Prob\UserIdToUrl\part-00000
+#D:\python\Prob\Urls_Json_Data.txt
+
+UserIdToGenderTrainKey='UserIdToGenderTrain.csv'
+UserIdToGenderTestKey='UserIdToGenderTest.csv'
+UrlsJsonDataKey='Urls_data.txt'
+Part1='UserIdToUrl\part-00003'
+
+TrnFile=pd.read_csv(UserIdToGenderTrainKey)
+TstFile=pd.read_csv(UserIdToGenderTestKey)
+
+#UrlsJsonDataFile=pd.read_json(UrlsJsonDataKey)
+#UserIdToGenderTrain.head()
+
+
+
+def readFile(fileName,countLines):
+    i=0
+    df=pd.DataFrame()
+    with open(fileName,'rb') as f:
+        for line in f.readlines():
+            i=i+1
+            if i==2:
+                df=pd.DataFrame((line.decode("utf-8")).split(','))
+            if i > 2:
+                stArr=(line.decode("utf-8")).split(',')
+                print(stArr)
+                df=df.append(stArr)
+                if i == countLines:
+                  break
+    return df
+
+GenderTrainDf=pd.DataFrame(TrnFile)
+GenderTestDf=pd.DataFrame(TstFile)
+
+TrnUserIdsLst=GenderTrainDf['userid'].values
+TrnGenderLst=GenderTrainDf['gender'].values
+TstUserIdsLst=GenderTestDf['userid'].values
+print(readFile(Part1,10))
